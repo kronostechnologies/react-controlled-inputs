c7be92ae435b084e703185d5668bb1875bd137c8
Alexandre Poirier <apoirier@kronostechnologies.com>
2017-12-01 16:44:50 -0500

    Expose the formatter through the lib package for use with non-inputs

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as Input } from './components/input';
 export { default as CurrencyInput } from './components/currency-input';
 export { default as PercentageInput } from './components/percentage-input';
 export { default as NumberInput } from './components/number-input';
+export { default as formatter } from './utils/formatter';
